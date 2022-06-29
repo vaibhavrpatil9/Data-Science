dcbb6f335621647d411d03cbd4f0bb2968a393b2
Patil <patilv18@asia.roche.com>
2022-06-29 12:52:37 +0530

    Assignmnet_24

---
+++ Assignment_no_24.py
@@ -0,0 +1,34 @@
+"""1. What is the relationship between def statements and lambda expressions ?
+Ans - def statemnts and lambda expression both are the functions while def statements are not single line functions and it
+contain list whie lambda expression are single line with no return statements.
+
+2. What is the benefit of lambda?
+Ans- One line functions. We use them to create anonymous functions.The code is simple and clear.No additional
+variables are added.
+
+3. Compare and contrast map, filter, and reduce.
+Ans - Map operation takes a mapping function and a vector of data as arguments and returns a new vector, which is the result of
+applying the mapping function on each element of the vector independently.
+The filter function operates on a list and returns a subset of that list after applying the filtering rule.
+The reduce function will transform a given list into a single value by applying a given function continuously
+to all the elements.
+
+4. What are function annotations, and how are they used?
+Ans - Function annotations are arbitrary python expressions that are associated with various part of functions. Function
+annotations provide a way of associating various parts of a function with arbitrary python expressions at compile time.
+
+5. What are recursive functions, and how are they used?
+Ans - A recursive function is a function in code that refers to itself for execution. If we want to call same function
+within the function then we will use recursive functions
+
+6. What are some general design guidelines for coding functions?
+Ans - Wrap lines so that they don’t exceed 79 characters
+    Use of regular and updated comments are valuable to both the coders and users.
+    Use of trailing commas
+    Use Python’s default UTF-8 or ASCII encodings and not any fancy encodings
+    Follow Naming Conventions
+    Name your classes and functions consistently
+
+7. Name three or more ways that functions can communicate results to a caller.
+Ans - Using Call by Value and Call by Reference.
+"""
